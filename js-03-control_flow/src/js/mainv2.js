d21516940cce2b39bd06341394f4f8da4d996aad
Jonathan Caixba <jonathan.caixba99@unach.mx>
2025-12-01 12:29:10 -0600

    feat: creo mi archivo mainv2 para mis cambios sin conflictos

---
+++ js-03-control_flow/src/js/mainv2.js
@@ -0,0 +1,25 @@
+/* const getWeatherSwitch = (codigo) => {
+    let message;
+    switch(codigo){
+        case 0:
+            currentWeather = "Clear Sky"
+            break;
+        case 1:
+        case 2:
+        case 3:
+            currentWeather = "Mainly clear, partly cloudy, and overcast";
+            break;
+        case 45:
+        case 48:
+            currentWeather = "Fog and depositing rime fog";
+            break;
+        default:
+            currentWeather = "default";
+    }
+    return currentWeather;
+}
+
+console.log(getWeatherSwitch(0)); // Clear Sky
+console.log(getWeatherSwitch(1)); // Mainly clear, partly cloudy, and overcast
+console.log(getWeatherSwitch(48)); // Fog and depositing rime fog
+console.log(getWeatherSwitch(88)); // default */
